fb7b39f1379c6d3fbcd18fc595ae38d4cb11872f
Bondarev-Ilya <ilya2002940@gmail.com>
2026-01-13 17:28:45 +0300

    api

--- Function/LED/led.h
+++ Function/LED/led.h
@@ -4,6 +4,9 @@
 #include <stm32f411xe.h>
 
 void led_init(void);
+void ledShowRgb(uint8_t r, uint8_t g, uint8_t b);
+void ledShowHsv(uint8_t h, uint8_t s, uint8_t v);
+void ledShowHue(uint8_t hue);
 
 
 #endif
